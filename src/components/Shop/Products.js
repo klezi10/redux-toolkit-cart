e982dc9517faec673853974359f022e44ad1830a
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-07-26 15:21:05 +0400

    added dummy data

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_DATA = [
+  {
+    id: "b1",
+    title: "My first product",
+    price: 10,
+    description: "This is my first product",
+  },
+  {
+    id: "b2",
+    title: "My second product",
+    price: 15,
+    description: "This is my second product",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_DATA.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
