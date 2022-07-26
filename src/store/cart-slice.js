ced9724946736bbbd8bd4fe964ad8a45e316dfbf
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-07-26 15:15:29 +0400

    added cart remove function slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -24,8 +24,15 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price
       }
     },
-    removeItems(state) {
-      state.quantity--;
+    removeItems(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
     },
   },
 });
