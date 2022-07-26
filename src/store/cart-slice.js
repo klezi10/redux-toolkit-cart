78c73d3a3dc4d3290230d110d978df1d2633e3c5
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-07-26 15:06:26 +0400

    set up addItems function reducer

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,11 +4,25 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
-    totalQuantity: 0
+    totalQuantity: 0,
+    // totalPrice: 0
    },
   reducers: {
     addItems(state, action) {
-      state.quantity++;
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        })
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+      }
     },
     removeItems(state) {
       state.quantity--;
